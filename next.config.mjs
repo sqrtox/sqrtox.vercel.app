a319a18e0b496da4c2596c98bdc87a48e211f2ab
sqrtox <84203131+sqrtox@users.noreply.github.com>
2022-09-01 00:35:50 +0900

    追加：Content-Security-Policyヘッダーを追加
    Signed-off-by: sqrtox <84203131+sqrtox@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 //@ts-check
 
+const isDev = process.env.NODE_ENV === 'development';
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -10,6 +12,18 @@ const nextConfig = {
     {
       source: '/:path*',
       headers: [
+        {
+          key: 'Cross-Origin-Embedder-Policy',
+          value: 'require-corp'
+        },
+        {
+          key: 'Cross-Origin-Opener-Policy',
+          value: 'same-origin'
+        },
+        {
+          key: 'Cross-Origin-Resource-Policy',
+          value: 'same-origin'
+        },
         {
           key: 'X-Frame-Options',
           value: 'DENY'
@@ -56,6 +70,25 @@ const nextConfig = {
             'serial=(), ' +
             'trust-token-redemption=(), ' +
             'window-placement=()'
+          )
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: (
+            isDev ? (
+              'default-src \'self\' \'unsafe-eval\'; ' +
+              'font-src https://fonts.gstatic.com https://cdn.jsdelivr.net; ' +
+              'style-src \'unsafe-inline\' https://fonts.googleapis.com https://cdn.jsdelivr.net; ' +
+              'style-src-elem \'unsafe-inline\' https://fonts.googleapis.com https://cdn.jsdelivr.net'
+            ) : (
+              'default-src \'self\'; ' +
+              'connect-src \'self\' https://www.google-analytics.com; ' +
+              'font-src https://cdn.jsdelivr.net https://fonts.gstatic.com; ' +
+              'style-src \'unsafe-inline\' \'self\' https://cdn.jsdelivr.net; ' +
+              'style-src-elem \'unsafe-inline\' \'self\' https://cdn.jsdelivr.net; ' +
+              'script-src \'unsafe-inline\' \'self\' https://www.googletagmanager.com; ' +
+              'script-src-elem \'unsafe-inline\' \'self\' https://www.googletagmanager.com'
+            )
           )
         }
       ]
