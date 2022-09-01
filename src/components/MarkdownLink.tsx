17b231944a4d10da161111cdda9de3f463454acd
sqrtox <84203131+sqrtox@users.noreply.github.com>
2022-09-02 00:16:07 +0900

    fix: fix markdown link nonce

--- src/components/MarkdownLink.tsx
+++ src/components/MarkdownLink.tsx
@@ -1,3 +1,5 @@
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import MuiLink from '@mui/material/Link';
 import NextLink from 'next/link';
@@ -6,18 +8,24 @@ import { isExternalLink } from '~/util/isExternalLink';
 
 const MarkdownLink: FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href = '/', children }) => {
   const externalLink = isExternalLink(href);
+  const cache = createCache({
+    key: 'css',
+    nonce: process.env.NEXT_PUBLIC_NONCE
+  });
 
   return (
-    <NextLink href={href} passHref>
-      <MuiLink
-        target={externalLink ? '_blank' : undefined}
-        rel={externalLink ? 'noopener noreferrer' : undefined}
-        color='primary'
-      >
-        {children}
-        {externalLink && <OpenInNewIcon sx={{ marginLeft: '0.1rem', verticalAlign: 'middle', fontSize: '1rem' }} />}
-      </MuiLink>
-    </NextLink>
+    <CacheProvider value={cache}>
+      <NextLink href={href} passHref>
+        <MuiLink
+          target={externalLink ? '_blank' : undefined}
+          rel={externalLink ? 'noopener noreferrer' : undefined}
+          color='primary'
+        >
+          {children}
+          {externalLink && <OpenInNewIcon sx={{ marginLeft: '0.1rem', verticalAlign: 'middle', fontSize: '1rem' }} />}
+        </MuiLink>
+      </NextLink>
+    </CacheProvider>
   );
 };
 
