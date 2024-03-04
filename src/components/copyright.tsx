c4c0a39c944ee83cbb30d3af8177d223452a49a0
sqrtox <84203131+sqrtox@users.noreply.github.com>
2024-03-05 01:24:06 +0900

    vercelの大文字小文字を修正するためにファイル名を変更

--- src/components/copyright.tsx
+++
@@ -1,9 +0,0 @@
-import Typography from "@mui/material/Typography";
-
-export default function Copyright() {
-  return (
-    <Typography color="text.secondary">
-      © 2022 sqrtox
-    </Typography>
-  );
-}
