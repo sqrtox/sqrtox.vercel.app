33072523cc5caf645f817d8a1c7c8278b3c254fd
sqrtox <84203131+sqrtox@users.noreply.github.com>
2024-07-18 20:39:35 +0900

    robots.txt and sitemap.xml

---
+++ src/app/sitemap.ts
@@ -0,0 +1,40 @@
+import { getAllArticles } from "@/utils/blog";
+import { ArticleVisibility } from "@/utils/blog/article";
+import { _definedTags } from "@/utils/blog/tag";
+
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const articles = await getAllArticles();
+
+  return [
+    ...articles.filter(article => article.visibility === ArticleVisibility.Public).map(article => ({
+      url: new URL(`/article/${article.slug}`, process.env.NEXT_PUBLIC_BASE_URL).href,
+      lastModified: new Date(article.modifiedTimestamp ?? article.publishedTimestamp)
+    })),
+    ...[..._definedTags.keys()].map(tagId => ({
+      url: new URL(`/tag/${tagId}`, process.env.NEXT_PUBLIC_BASE_URL).href
+    })),
+    {
+      url: process.env.NEXT_PUBLIC_BASE_URL as string,
+    }
+    // {
+    //   url: "https://acme.com",
+    //   lastModified: new Date(),
+    //   changeFrequency: "yearly",
+    //   priority: 1,
+    // },
+    // {
+    //   url: "https://acme.com/about",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: "https://acme.com/blog",
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 0.5,
+    // },
+  ];
+}
