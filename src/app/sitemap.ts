a469faf2af9bf51c34e177913eacba9618b88078
sqrtox <84203131+sqrtox@users.noreply.github.com>
2024-07-19 12:58:48 +0900

    remove comments

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -18,23 +18,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: process.env.NEXT_PUBLIC_BASE_URL as string,
     }
-    // {
-    //   url: "https://acme.com",
-    //   lastModified: new Date(),
-    //   changeFrequency: "yearly",
-    //   priority: 1,
-    // },
-    // {
-    //   url: "https://acme.com/about",
-    //   lastModified: new Date(),
-    //   changeFrequency: "monthly",
-    //   priority: 0.8,
-    // },
-    // {
-    //   url: "https://acme.com/blog",
-    //   lastModified: new Date(),
-    //   changeFrequency: "weekly",
-    //   priority: 0.5,
-    // },
   ];
 }
