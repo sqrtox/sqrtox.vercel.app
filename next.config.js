09097c2e301fb69685076e52d997fb48a35c50bd
sqrtox <84203131+sqrtox@users.noreply.github.com>
2024-07-20 02:42:56 +0900

    fix webpack

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 // @ts-check
 
+const dev = process.env.NODE_ENV === "development";
+
+/**
+ * @typedef {import("webpack").Configuration} Configuration
+ */
+
 /**
  * @type {import("next").NextConfig}
  */
@@ -15,6 +21,30 @@ const config = {
       },
     },
   },
+
+  webpack: dev
+    ? undefined
+    : /**
+       * @param {Configuration} config
+       * @returns {Configuration}
+       */
+      (config) => {
+        config.resolve ??= {};
+        config.resolve.alias ??= {};
+
+        const budouxAlias = "budoux/dist/index";
+
+        if (Array.isArray(config.resolve.alias)) {
+          config.resolve.alias.push({
+            name: "budoux",
+            alias: budouxAlias,
+          });
+        } else {
+          config.resolve.alias.budoux = budouxAlias;
+        }
+
+        return config;
+      },
 };
 
 export default config;
