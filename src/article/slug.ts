25230e4c5b0079ce50b46a9e1b24e82f03be3771
sqrtox <84203131+sqrtox@users.noreply.github.com>
2025-05-17 15:14:14 +0900

    fix エラーメッセージをより詳細に

--- src/article/slug.ts
+++ src/article/slug.ts
@@ -9,7 +9,7 @@ export type SafeSlug = Brand<string, "safeSlug">;
 type ValidateSlug = (slug: UnsafeSlug) => asserts slug is SafeSlug;
 
 export const validateSlug: ValidateSlug = (slug) => {
-  const invalidSlug = () => new Error("Invalid slug");
+  const invalidSlug = () => new Error(`Invalid slug "${slug}"`);
 
   if (typeof slug !== "string") throw invalidSlug();
   if (!SLUG.test(slug)) throw invalidSlug();
